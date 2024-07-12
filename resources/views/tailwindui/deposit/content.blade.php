9a9821e334ced8eeae3be5531f7111ef086c6789
frutti-art <frutti101@outlook.com>
2024-07-12 15:59:39 -0700

    asdasdas

--- resources/views/tailwindui/deposit/content.blade.php
+++ resources/views/tailwindui/deposit/content.blade.php
@@ -20,7 +20,7 @@
                               class="block w-full min-h-24 md:min-h-16 overflow-hidden resize-y rounded-none rounded-l-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">{{ $user->wallet_address }}</textarea>
                     </div>
                     <button type="button" onclick="copyToClipboard()"
-                            class="dark:text-[#FFFFFF] relative -ml-px inline-flex items-center rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-100">
+                            class="dark:text-[#FFFFFF] relative -ml-px inline-flex items-center rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                              stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
